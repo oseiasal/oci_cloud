56ee44cfa209297293b3f5793cbb50f8d12f1a30
Oséias Nascimento <oseiasal@gmail.com>
2025-04-16 07:48:18 -0300

    fix: ajustes de ci

--- terraform/n8n_machine/provider.tf
+++ terraform/n8n_machine/provider.tf
@@ -10,7 +10,7 @@ provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user
   fingerprint      = var.fingerprint
-  private_key      = var.key_file
+  private_key_path      = var.key_file
   region           = var.region
   
 }
