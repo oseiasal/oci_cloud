642ea699abcc2610d7d393fce77fa8fbcef44124
Oséias Nascimento <oseiasal@gmail.com>
2025-04-19 20:26:43 -0300

    refatorar estrutura

--- environments/n8n_machine/terraform/main.tf
+++ environments/n8n_machine/terraform/main.tf
@@ -1,5 +1,5 @@
 module "n8n_machine" {
-    source = "../modules/instance"
+    source = "../../../modules/instance"
     compartiment_id = var.compartiment_id
     subnet = module.network.subnet_id
     totalCPUs = 1
@@ -9,14 +9,14 @@ module "n8n_machine" {
 }
 
 module "network" {
-    source = "../modules/vcn"
+    source = "../../../modules/vcn"
     compartiment_id = var.compartiment_id
     tenancy_ocid = var.compartiment_id
     ports = [22, 80, 81, 443]
 }
 
 module "createfile" {
-    source = "../modules/createfile"
+    source = "../../../modules/createfile"
     machine_name = "n8n_machine"
     machine_ip = module.n8n_machine.ip_da_maquina
 }
